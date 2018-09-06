585446db68b61a5868d1507edd38ccb1449b1d4a
mraviel <mraviel@outlook.com>
2018-09-06 09:57:21 +0300

    OOK

--- client_template.py
+++ client_template.py
@@ -1,62 +1,87 @@
-#   Heights sockets Ex. 2.7 template - client side
-#   Author: Barak Gonen, 2017
-
-
 import socket
+import shutil
+import os
+import sys
+import signal  # Allow socket destruction on Ctrl+C
 
-IP = '127.0.0.1'
-PORT = 8800
+class Client(object):
+    """ The client class """
 
-commands_list = ['TAKE_SCREENSHOT', 'SEND_FILE', 'DIR', 'DELETE', 'COPY', 'EXECUTE', 'EXIT']
+    def __init__(self, PORT=8800, IP='127.0.0.1'):
 
-def valid_request(request):
-    """Check if the request is valid (is included in the available commands)
+        self.PORT = PORT
+        self.IP = IP
+        self.commands_list = ['TAKE_SCREENSHOT', 'SEND_FILE', 'DIR', 'DELETE', 'COPY', 'EXECUTE', 'EXIT']
 
-    Return:
-        True if valid, False if not
-    """
-    for command in commands_list:
-        if request == command:
-            return True
-    return False
+    def start(self):
+        """ To start the client """
 
-def send_request_to_server(my_socket, request):
-    """Send the request to the server. First the length of the request (2 digits), then the request itself
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    Example: '04EXIT'
-    Example: '12DIR c:\cyber'
-    """
-    my_socket.send(request)
+        try:
+            client_socket.connect((self.IP, self.PORT))
+
+        except Exception as e:
+            print("Error: Could not connect to port {0}".format(self.PORT))
+            self.shutdown()
+            sys.exit(1)
+
+        self.main(client_socket)
+
+    def shutdown(self):
+        """ Shuts down the client """
+
+        try:
+            print ("Shuting down the client")
+            Client.shutdown(socket.SHUT_RDWR)
+
+        except Exception as e:
+            pass  # Pass if socket is already closed
+
+    def valid_request(self, request):
+        """Check if the request is valid (is included in the available commands)"""
+
+        for command in self.commands_list:
+            if command in request:
+                return True
+        return False
 
 
-def handle_server_response(my_socket, request):
-    """Receive the response from the server and handle it, according to the request
 
-    For example, DIR should result in printing the contents to the screen,
-    while SEND_FILE should result in saving the received file and notifying the user
-    """
-    data = my_socket.recv(1024)
-    print data
+    def send_request_to_server(self, my_socket, request):
+        """Send the request to the server."""
 
-def main():
-    # open socket with the server
-    my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    my_socket.connect((IP, PORT))
+        my_socket.send(request)
 
-    # print instructions
-    print('Welcome to remote computer application. Available commands are:\n')
-    print('TAKE_SCREENSHOT\nSEND_FILE\nDIR\nDELETE\nCOPY\nEXECUTE\nEXIT')
 
-    done = False
-    # loop until user requested to exit
-    while not done:
-        request = raw_input("Please enter command:\n")
-        if valid_request(request):
-            send_request_to_server(my_socket, request)
-            handle_server_response(my_socket, request)
-            if request == 'EXIT':
-                done = True
-    my_socket.close()
+    def handle_server_response(self, my_socket, request):
+        """Receive the response from the server and handle it, according to the request"""
+
+        data = my_socket.recv(1024)
+        print data
+
+    def main(self, client_socket):
+        """ The main in the class """
+
+        # print instructions
+        print('Welcome to remote computer application. Available commands are:\n')
+        print('TAKE_SCREENSHOT\nSEND_FILE\nDIR\nDELETE\nCOPY\nEXECUTE\nEXIT')
+
+        done = False
+        # loop until user requested to exit
+        while not done:
+            request = raw_input("Please enter command:\n")
+            if self.valid_request(request):
+                self.send_request_to_server(client_socket, request)
+                self.handle_server_response(client_socket, request)
+                if request == 'EXIT':
+                    done = True
+        client_socket.close()
+
 
 if __name__ == '__main__':
-    main()
+
+    my_client = Client()
+
+    my_client.start()
+    print("Press Ctrl+C to shut down server.")
--- server_template.py
+++ server_template.py
@@ -1,100 +1,197 @@
-#   Heights sockets Ex. 2.7 template - server side
-#   Author: Barak Gonen, 2017
-
 import socket
-
-IP = '0.0.0.0'
-PORT = 8800
-
-
-def receive_client_request(client_socket):
-    """Receives the full message sent by the client
-
-    Works with the protocol defined in the client's "send_request_to_server" function
-
-    Returns:
-        command: such as DIR, EXIT, SCREENSHOT etc
-        params: the parameters of the command
-
-    Example: 12DIR c:\cyber as input will result in command = 'DIR', params = 'c:\cyber'
-    """
-    data = client_socket.recv(1024)
-    list_data = data.split()
-    command = list_data[0]
-
-    try:
-        params = list_data[1]
-        if len(list_data) > 2:
-            params = ' '.join(list_data[1:])
-    except IndexError:
-        params = None
-
-    return (command, params)
+import shutil
+import os
+import sys
+import signal  # Allow socket destruction on Ctrl+C
 
 
-def check_client_request(command, params):
-    """Check if the params are good.
+class Server(object):
+    """ The server class """
 
-    For example, the filename to be copied actually exists
+    def __init__(self, PORT=8800, IP='0.0.0.0'):
+        self.PORT = PORT
+        self.IP = IP
+        self.commands_list = ['TAKE_SCREENSHOT', 'SEND_FILE', 'DIR', 'DELETE', 'COPY', 'EXECUTE', 'EXIT']
 
-    Returns:
-        valid: True/False
-        error_msg: None if all is OK, otherwise some error message
-    """
-    if params == None:
-        valid = True
-        error_msg = None
-    else:
-        valid = False
-        error_msg = 'Eroor'
-    return valid, error_msg
+    def start(self):
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+        try:
+            print ("The server is start on host:{0}:{1}").format(self.IP, self.PORT)
+            server_socket.bind((self.IP, self.PORT))
+            print ("Server started on port: {0}").format(self.PORT)
 
-def handle_client_request(command, params):
-    """Create the response to the client, given the command is legal and params are OK
+        except Exception as e:
+            print("Error: Could not bind to port {0}".format(self.PORT))
+            self.shutdown()
+            sys.exit(1)
 
-    For example, return the list of filenames in a directory
+        self.main(server_socket)
 
-    Returns:
-        response: the requested data
-    """
+    def shutdown(self):
+        """ Shuts down the server """
 
-    return 'EveryThing Works GOOD!'
+        try:
+            print ("Shuting down the server")
+            Server.shutdown(socket.SHUT_RDWR)
 
+        except Exception as e:
+            pass  # Pass if socket is already closed
 
-def send_response_to_client(response, client_socket):
-    """Create a protocol which sends the response to the client
+    def receive_client_request(self, client_socket):
 
-    The protocol should be able to handle short responses as well as files
-    (for example when needed to send the screenshot to the client)
-    """
-    client_socket.send(response)
+        """ Receives the full message sent by the client """
 
+        data = client_socket.recv(1024)
+        list_data = data.split()
+        command = [list_data[0]]  # the command from the self.commands_list (command = list of one command)
 
+        try:  # check if there is params. (params = list of params)
+            params = [list_data[1]]
+            if len(list_data) > 2:
+                params = list_data[1:]
 
-def main():
-    # open socket with client
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((IP, PORT))
-    server_socket.listen(1)
-    client_socket, address = server_socket.accept()
+        except IndexError:
+            params = None
 
-    # handle requests until user asks to exit
-    done = False
-    while not done:
-        command, params = receive_client_request(client_socket)
-        valid, error_msg = check_client_request(command, params)
-        if valid:
-            response = handle_client_request(command, params)
-            send_response_to_client(response, client_socket)
-        else:
-            send_response_to_client(error_msg, client_socket)
+        return (command, params)
 
-        if command == 'EXIT':
-            done = True
+    def check_client_request(self, command, params):
+        """ check if params are good. """
 
-    client_socket.close()
-    server_socket.close()
+        command_str = ' '.join(command)  # str of command from self.commands_list
+
+        valid = True  # defualt
+        error_msg = None  # defualt
+
+        if command_str == 'DIR':
+            try:  # find the dir
+                valid_dir = os.path.isdir(params[0])
+                if valid_dir == False:
+                    valid = False
+                    error_msg = 'Error'
+
+            except TypeError:
+                valid = False
+                error_msg = 'Error'
+            return valid, error_msg
+
+        elif command_str == 'DELETE':
+            try:  # find the file
+                valid_file = os.path.isfile(params[0])
+                if valid_file == False:
+                    valid = False
+                    error_msg = 'Error'
+
+            except TypeError:
+                valid = False
+                error_msg = 'Error'
+            return valid, error_msg
+
+        elif command_str == 'COPY':
+            if params == None:
+                return valid, error_msg
+
+            for i in params:
+                valid_dir = os.path.isdir(i)
+                if valid_dir == False:  # check if it's dir
+                    valid_file = os.path.isfile(i)
+                    if valid_file == False:  # check if it's file
+                        valid = False
+                        error_msg = 'Error'
+                    else:
+                        valid = True
+                        error_msg = None
+            return valid, error_msg
+
+        elif command_str == 'EXECUTE' or command_str == 'EXIT' or command_str == 'TAKE_SCREENSHOT':
+            return valid, error_msg
+
+    def handle_client_request(self, command, params):
+        """ Create the response to the client, given the command is legal and params are OK """
+
+        command_str = ' '.join(command)
+
+        try:
+            params_str = ' '.join(params)
+        except TypeError:
+            params_str = params
+
+        if (command_str == 'TAKE_SCREENSHOT'):
+            os.system("import -window root temp.png")
+            return 'The image was taken!'
+
+        elif (command_str == 'SEND_FILE'):
+            pass
+
+        elif (command_str == 'DIR'):
+            list_dir = os.listdir(params_str)
+            return str(list_dir)
+
+        elif (command_str == 'DELETE'):
+            os.remove(params_str)
+            return 'The file in location: {0} is deleted'.format(params_str)
+
+        elif (command_str == 'COPY'):
+            try:
+                shutil.copy(params[0], params[1])
+                return "The file is copied"
+            except TypeError:
+                return "You need to give 2 arguments!"
+            except IndexError:
+                return "You need to give 2 arguments!"
+
+        elif (command_str == 'EXECUTE'):
+            try:
+                os.system(params_str)
+                return "tHe EXECUTE run perfectly"
+            except TypeError:
+                return "There is No argument!"
+
+        elif (command_str == 'EXIT'):
+            return 'Good bye'
+
+    def send_response_to_client(self, response, client_socket):
+        """ send respone to the client """
+        client_socket.send(response)
+
+    def main(self, server_socket):
+
+        # open socket with client
+        server_socket.listen(1)
+        client_socket, address = server_socket.accept()
+
+        # handle requests until user asks to exit
+        done = False
+        while not done:
+            command, params = self.receive_client_request(client_socket)
+            valid, error_msg = self.check_client_request(command, params)
+            if valid:
+                response = self.handle_client_request(command, params)
+                self.send_response_to_client(response, client_socket)
+            else:
+                self.send_response_to_client(error_msg, client_socket)
+
+            if command == 'EXIT':
+                done = True
+
+        client_socket.close()
+        server_socket.close()
+
 
 if __name__ == '__main__':
-    main()
+
+    my_server = Server()
+
+
+    def shutdownServer(sig, unused):
+        """
+        Shutsdown server from a SIGINT recieved signal
+        """
+        my_server.shutdown()
+        sys.exit(1)
+
+
+    signal.signal(signal.SIGINT, shutdownServer)
+    my_server.start()
+    print("Press Ctrl+C to shut down server.")
